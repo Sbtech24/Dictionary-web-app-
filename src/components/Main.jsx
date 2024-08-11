19fe4237ed4a06c57ef356db22788eec5439cfe7
sbtech24 <bajomosemilore@gmail.com>
2024-08-11 17:07:57 +0100

    Added loading and error handling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,78 +1,105 @@
-import { useState,useEffect } from "react"
-import "./main.css"
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import { Oval } from "react-loader-spinner";
+import "./main.css";
+import axios from "axios";
 
+export default function Main({ search, play }) {
+  const [inputValue, setInputValue] = useState("");
+  const [data, setData] = useState({});
+  const [showMeaning, setShowMeaning] = useState(false);
+  const [displayPlay, setDisplayPlay] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error,setError] = useState("")
 
-export default function Main({search,play}){
-    const [inputValue,setInputValue] = useState("")
-    const [data,setData] = useState({})
-    const [showMeaning, setShowMeaning] = useState(false)
-    const [displayPlay,setDisplayPlay] =useState(false)
-   
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`
+      );
+     
+     setData(response.data[0])
+     setLoading(false)
+     setInputValue("")
+      
+      setShowMeaning(true);
+      setDisplayPlay(true);
+      //   console.log(data.word)
+    } catch (error) {
+      setError(error)
+      setInputValue("")
+      setLoading(false)
+    }
+  };
 
-    const fetchData = async () => {
-        try {
-          const response = await axios.get(
-            `https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`
-          );
-          setData(response.data[0]);
-          setShowMeaning(true)
-          setDisplayPlay(true)
-        //   console.log(data.word)
-          
-        } catch (error) {
-          console.error('Error fetching data:', error);
-          
-        }
-      };
+  function handleClick(e) {
+    e.preventDefault();
+  }
+  return (
+    <>
+    {loading ? (
+      <div className="flex justify-center">
+      <Oval
+        visible={true}
+        height="40"
+        width="40"
+        color="#A445ED"
+        secondaryColor="#A445ED"
+        ariaLabel="oval-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+      </div>
+    ) : (
+    <main className="">
+      <form className="mb-4 flex justify-between gap-2" onSubmit={handleClick}>
+        <input
+          type="text"
+          className={
+            "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline " +
+            " input-box"
+          }
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter a word"
+        />
 
-function handleClick(e){
-    e.preventDefault()
-    
+        <button onClick={fetchData}>
+          <img src={search} alt="" />
+        </button>
+      </form>
+          {error ? <h1>Not found pls try again </h1>:
+      <div className="my-5">
+        <div className="flex justify-between">
+          <h1 className="text-5xl">{data.word}</h1>
+          {displayPlay ? <img src={play} alt="" width={50} /> : <></>}
+        </div>
+        {data.phonetics && data.phonetics.length > 0 && (
+          <div>
+            <h3>Phonetics</h3>
+            {data.phonetics.slice(0, 2).map((phonetic, index) => (
+              <p key={index}>{phonetic.text}</p>
+            ))}
+          </div>
+        )}
+      </div>
 }
-  return(
-        <main className="">
-            <form className="mb-4 flex justify-between gap-2"onSubmit={handleClick} >
-                <input type="text" className={"shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline " + " input-box"} 
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                placeholder="Enter a word"/>
-                
-                <button onClick={fetchData}><img src={search} alt="" /></button>
-               
-            </form>
-                    <div className="my-5">
-                <div className="flex justify-between ">
-        
-                <h1 className="text-5xl">{data.word}</h1>
-                {displayPlay ? <img src={play} alt="" width={50} />: <></>}
-                </div>
-                {data.phonetics && data.phonetics.length > 0 && (
-            <div>
-              <h3>Phonetics</h3>
-              {data.phonetics.slice(0,2).map((phonetic, index) => (
-                <p key={index}>{phonetic.text}</p>
+      {showMeaning && <h3>Meanings</h3>}
+      {data.meanings &&
+        data.meanings.slice(0, 2).map((meaning, index) => (
+          <div key={index}>
+            <p>{meaning?.partOfSpeech}</p>
+            <ul>
+              {meaning.definitions.slice(0, 3).map((definition, idx) => (
+                <li key={idx}>{definition.definition}</li>
               ))}
-            </div>
-          )}
-            </div>
+            </ul>
+          </div>
+        ))}
 
-            
-            {showMeaning && <h3>Meanings</h3>}
-            {data.meanings &&
-              data.meanings.slice(0, 2).map((meaning, index) => (
-                <div key={index}>
-                
-                  <p>{meaning?.partOfSpeech}</p>
-                  <ul>
-                    {meaning.definitions.slice(0,3).map((definition, idx) => (
-                      <li key={idx}>{definition.definition}</li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-
-              
-        </main>
-    )
+      
+    </main>
+      )}
+  </>
+  );
 }
